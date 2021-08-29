14bbb151d4aa7d868e7b742aaee4b2990d7785bf
Nikolay <forsh_nikolay@mail.ru>
2021-08-29 14:20:24 +0300

    Refactor Tests

---
+++ src/main/java/ru/forsh/voting_system_for_restaurants/web/converter/LocalDateFormatter.java
@@ -0,0 +1,20 @@
+package ru.forsh.voting_system_for_restaurants.web.converter;
+
+import org.springframework.format.Formatter;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class LocalDateFormatter implements Formatter<LocalDate> {
+
+    @Override
+    public LocalDate parse(String text, Locale locale) {
+        return parseLocalDate(text);
+    }
+
+    @Override
+    public String print(LocalDate lt, Locale locale) {
+        return lt.format(DateTimeFormatter.ISO_LOCAL_DATE);
+    }
+}
